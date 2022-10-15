05f79441eab702bc55b1d9ba2a7b30f87183f5ef
sikander-afzaal <sikander.aaa@gmail.com>
2022-10-15 19:38:10 +0500

    asdasd

--- src/Pages/Landing/anim.js
+++ src/Pages/Landing/anim.js
@@ -58,8 +58,8 @@ export const animationToTop = () => {
     document.querySelectorAll(".scroll-section").forEach((section, i) => {
       ScrollTrigger.create({
         trigger: section,
-        start: "top bottom-=1",
-        end: "bottom top+=1",
+        start: "top 95%",
+        end: "bottom 5%",
         onEnter: () => goToSection(section),
         onEnterBack: () => goToSection(section),
       });
